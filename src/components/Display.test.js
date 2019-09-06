5239b29a2b7e6288fba54a210e8ab348cac3fdb7
RichardEsquivel <rlesquivel@gmail.com>
2019-09-06 07:58:06 -0500

    Basic tests running successfully

--- src/components/Display.test.js
+++ src/components/Display.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Display from './Display.js';
 
-//creating basic render test for the Display component
-
-test('renders without crashing', () => {
-	render(<Display />);
+//creating basic render test for the Display component describe allows us to create a block that groups together several related tests
+describe('<Display/>', () => {
+	test('renders without crashing', () => {
+		render(<Display />);
+	})
+	test('props work', () => {
+		render(<Display balls={3} strikes={2} />)
+	})
 });
